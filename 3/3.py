63786b0498f66268946a514c9fc26dfe4b2db9c1
Madelaine Boyd <madelaine.boyd+github@gmail.com>
2021-12-07 21:00:54 -0800

    3 prog

--- 3/3.py
+++ 3/3.py
@@ -1,4 +1,11 @@
 
+def most_common_bits(lns):
+    ones_ct = [0]*ln_len
+    for ln in lns:
+        for i in range(len(ln)):
+            ones_ct[i] += int(ln[i])
+    return ones_ct
+
 def main_a():
     ln_len = 12
     num_lines = 0
@@ -24,11 +31,29 @@ def main_a():
         epsilon = 2**ln_len - 1 - gamma
         print(f"G: {gamma}. E: {epsilon}. Power: {gamma*epsilon}")
 
+def mcb_at_ind(lns, i):
+    ones_ct = 0
+    num_lines = 0
+    for ln in lns:
+        num_lines += 1
+        ones_ct += int(ln[i])
+    return 1 if ones_ct >= num_lines //2 else 0
+
+
+
 def main_b():
     ln_len = 12
     ones_ct = [0]*ln_len
     num_lines = 0
     with open('input.txt') as inputf:
+        lns = inputf.readlines()
+        for i in range(ln_len):
+            mcb = mcb_at_ind(i)
+            lns = filter(...)
+            if len(lns) == 1:
+                return lns[0]
+
+
 
 
 if __name__ == "__main__":

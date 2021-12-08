5c1a38c6187208b87fa702d41aefa78cdda6b239
Madelaine Boyd <madelaine.boyd+github@gmail.com>
2021-12-07 20:57:29 -0800

    3 progress

---
+++ 3/3.py
@@ -0,0 +1,35 @@
+
+def main_a():
+    ln_len = 12
+    num_lines = 0
+    ones_ct = []
+    with open('input.txt') as inputf:
+        for ln in inputf:
+            num_lines += 1
+            ln = ln.strip("\n")
+            if num_lines == 1:
+                if len(ln) != ln_len:
+                    ln_len = len(ln)
+                    print(f"Setting line length to {len(ln)}")
+                ones_ct = [0]*ln_len
+            for i in range(len(ln)):
+                ones_ct[i] += int(ln[i])
+        gamma_str = ""
+        mid_lvl = num_lines // 2
+        print(f"Ones: {ones_ct}. Lines: {num_lines}")
+        for i in range(len(ones_ct)):
+            gamma_str += "1" if ones_ct[i] > mid_lvl else "0"
+
+        gamma = int(gamma_str, base=2)
+        epsilon = 2**ln_len - 1 - gamma
+        print(f"G: {gamma}. E: {epsilon}. Power: {gamma*epsilon}")
+
+def main_b():
+    ln_len = 12
+    ones_ct = [0]*ln_len
+    num_lines = 0
+    with open('input.txt') as inputf:
+
+
+if __name__ == "__main__":
+    main_b()
